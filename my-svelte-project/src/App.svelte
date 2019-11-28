f8dc426d47698d219d560c48dfcac3cdb0f3d8ad
Muthu <muthu.balakrishnan@driscolls.com>
2019-11-28 14:54:13 +0530

    updated
    updated

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,30 +1,42 @@
 <script>
-	export let name;
+	let search = '';
+	let loading = false;
+	const API_URL = 'https://api.giphy.com/v1/gifs/search?api_key=JnxTmEGKXjZeUKBzRjTQoMDg8OX8pS5U&rating=pg&q=';
+	let gifs = [];
+	async function formSubmitted(event) {
+		event.preventDefault();
+		loading = true;
+		gifs = [];
+		const url = `${API_URL}${search}`;
+		const response = await fetch(url);
+		const json = await response.json();
+		gifs = json.data.map(gif => gif.images.fixed_height.url);
+		loading = false;
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+.results {
+	column-count: 3;
+}
+img {
+	width: 100%;
+	height: auto;
+}
 </style>
+
+<form on:submit={formSubmitted}>
+	<label for="search">Search</label>
+	<input bind:value={search} id="search" name="search" />
+	<button type="submit">GO</button>
+</form>
+
+{#if loading}
+	<img alt="loading" src="https://media2.giphy.com/media/l3nWhI38IWDofyDrW/giphy.gif?cid=790b76115d055ab7424f75514dcb4d7a&rid=giphy.gif" />
+{/if}
+
+<div class="results">
+	{#each gifs as gif}
+		<img alt="gif" src={gif} />
+	{/each}
+</div>
